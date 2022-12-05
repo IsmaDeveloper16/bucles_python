d55ed531e93fcc586d7ab57030c5c66056bb1371
IsmaDeveloper16 <117777526+IsmaDeveloper16@users.noreply.github.com>
2022-12-05 19:53:49 -0300

    Add files via upload

---
+++ bucles_python-master/ejemplos_clase/ejemplo_1.py
@@ -0,0 +1,21 @@
+# Bucles [Python]
+# Ejemplos de clase
+
+# Autor: Inove Coding School
+# Version: 3.0
+
+# Ejemplos con bucle for y listas
+
+# Comenzar con una lista vacia de colores
+# Objetivo --> crear una lista con un bucle
+colores = []
+
+# Profe:
+# Crear un bucle for que se ejecute por 3 iteraciones
+# En cada iteración solicitar un color por consola
+# y agregarlo (append) a la lista de colores
+
+
+
+# Imprimir en pantalla la lista de colores
+
---
+++ bucles_python-master/ejemplos_clase/ejemplo_4.py
@@ -0,0 +1,29 @@
+# Bucles [Python]
+# Ejemplos de clase
+
+# Autor: Inove Coding School
+# Version: 3.0
+
+# Ejemplos con bucle for, listas y
+# operador incremento (+=)
+
+# Comenzar con una lista de edades
+# Objetivo --> calcular el promedio de todas las edades
+# utilizando un bucle y el operador incremento (+=)
+edades = [35, 38, 55, 33]
+
+# Profe:
+# Utilizar un bucle for sobre la lista de edades
+# acumule en la variable "suma_edades" (+=)
+# la suma de todas las edades
+
+
+# Utilizar la función len para determinar
+# cuantos elementos hay en la lista edades
+
+
+# Calcular el promedio de las edades:
+
+
+# Imprimir en pantalla el promedio de edades
+
